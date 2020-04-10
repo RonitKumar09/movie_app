dcf257c257b7408f47d46ceed0ec6e27d00b2551
RonitKumar09 <ronitkumar.robot@gmail.com>
2020-04-11 01:24:31 +0530

    final built
    movie app

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-let express = require('express');
+const express = require('express');
+const path = require('path');
+const request = require('request');
+require('dotenv').config();
+const app =express();
+
+app.set("views", path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get("/search", (req,res)=>{
+    res.render('search');
+});
+
+app.get("/results", (req,res)=>{
+    const api_key = process.env.API_KEY;
+    let query = req.query.search;
+    request(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=`+query, (error, response, body)=>{
+       if(error){ console.log (error); }
+       
+        let data = JSON.parse(body);
+        res.render("results", {data:data, query:query});
+        // console.log(data);
+    });
+});
+
+app.listen(8080, ()=>{
+    console.log("port started on port no 8080");
+});
