19d040b64a99268ed928f2ce793503809c8e19fb
RonitKumar09 <ronitkumar.robot@gmail.com>
2020-05-10 23:07:24 +0530

    route changes

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
+app.get('/', (req,res)=>{
+    res.redirect('/search');
+});
+
 app.get("/search", (req, res) => {
     res.render('search');
 });
@@ -16,18 +20,24 @@ app.get("/search", (req, res) => {
 app.get("/results", (req, res) => {
     const api_key = process.env.API_KEY;
     let query = req.query.search;
-    request(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=` + query, (error, response, body) => {
-        if (error) {
-            console.log(error);
-        }
 
-        let data = JSON.parse(body);
-        res.render("results", {
-            data: data,
-            query: query
+    if(query){
+        request(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=` + query, (error, response, body) => {
+            if (error) {
+                console.log(error);
+            }
+
+            let data = JSON.parse(body);
+            res.render("results", {
+                data: data,
+                query: query
+            });
+            // console.log(data);
         });
-        // console.log(data);
-    });
+    }
+    else{
+        res.redirect('/search');
+    }
 });
 
 app.listen(process.env.PORT, () => {
